4b3bbaa83dfa8a5588a7a99d64cfce573dd85504
ftinoco <ftinoco@ia.com.mx>
2019-08-06 16:40:13 -0500

    develop: custom server helper for parceljs flux with PUG

---
+++ server.js
@@ -0,0 +1,47 @@
+const Bundler = require('parcel-bundler')
+const app = require('express')()
+const path = require('path')
+const fs = require('fs')
+
+let isFirstBuild = true
+
+const serverDir = './dist'
+
+const bundler = new Bundler('./src/*.pug', {
+  sourceMaps: false,
+  cache: false
+})
+
+bundler.on('buildEnd', bundle => {
+  if (!isFirstBuild) return
+
+  fs.readdir(serverDir, (err, files) => {
+    if (err) throw err
+
+    const html = []
+
+    files.forEach(file => {
+      // only HTML files
+      if (file.match(/.+\.(html)$/)) {
+        html.push(file)
+      }
+    })
+
+    if (html.length === 0) throw new Error('Not HTML files found')
+
+    html.forEach(file => {
+      const fileName = file.replace('.html', '')
+
+      app.get(`/${fileName === 'index' ? '' : fileName}`, (req, res) => {
+        res.sendFile(path.join(__dirname, `dist/${file}`))
+      })
+    })
+  })
+
+  isFirstBuild = false
+})
+
+app.use(bundler.middleware())
+app.listen(8080)
+
+console.log('Running at Port 3000')
