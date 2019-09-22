f45cda2243f5782dddcdf67ebbdb129d500d14e4
Francisco Javier Tinoco Pérez <ftinoco@ia.com.mx>
2019-09-22 14:07:29 -0500

    refactor: better timing and entry strategy for better ux

--- src/js/modules/splash.js
+++ src/js/modules/splash.js
@@ -1,6 +1,5 @@
-import '../lib/DrawSVGPlugin'
 import { TimelineMax } from 'gsap'
-import { $, $$ } from '../tools/utils'
+import { $, $$, pubsub } from '../tools/utils'
 
 export default class Splash {
   constructor () {
@@ -12,25 +11,24 @@ export default class Splash {
   }
 
   init () {
-    const { splash, svg, paths } = this.DOM
+    const { splash, svg } = this.DOM
 
     const tl = new TimelineMax()
     tl
       .set(svg, { scale: 0.98 })
-      .set(paths, { drawSVG: '0%' })
       .to(svg, 1, {
         opacity: 1,
         scale: 1,
         ease: 'Expo.easeInOut'
       })
-      .staggerTo(paths, 1, {
-        drawSVG: '100%'
-      }, 0.05)
       .to(splash, 1, {
         autoAlpha: 0,
         scale: 0.98,
-        delay: 1,
+        delay: 0.1,
         ease: 'Expo.easeInOut'
+      })
+      .eventCallback('onComplete', () => {
+        pubsub.publish('entry:trigger')
       })
   }
 }
